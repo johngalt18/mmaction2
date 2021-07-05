8add58f12cbcadf1a7b834bcc35276555e7cd2b0
Юрий Самусевич <samusevich@useit.pro>
2021-07-05 17:59:20 +0500

    [UPD] added f-beta

--- mmaction/core/evaluation/__init__.py
+++ mmaction/core/evaluation/__init__.py
@@ -3,12 +3,14 @@ from .accuracy import (average_precision_at_temporal_iou,
                        get_weighted_score, interpolated_precision_recall,
                        mean_average_precision, mean_class_accuracy,
                        mmit_mean_average_precision, pairwise_temporal_iou,
-                       softmax, top_k_accuracy)
+                       softmax, top_k_accuracy,
+                       precision, recall, fhalf_score, compute_precision_recall)
 from .eval_detection import ActivityNetLocalization
 from .eval_hooks import DistEvalHook, EvalHook
 
 __all__ = [
     'DistEvalHook', 'EvalHook', 'top_k_accuracy', 'mean_class_accuracy',
+    'precision', 'recall', 'fhalf_score', 'compute_precision_recall',
     'confusion_matrix', 'mean_average_precision', 'get_weighted_score',
     'average_recall_at_avg_proposals', 'pairwise_temporal_iou',
     'average_precision_at_temporal_iou', 'ActivityNetLocalization', 'softmax',
